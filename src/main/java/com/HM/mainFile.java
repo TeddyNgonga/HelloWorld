7118ad0099a0a339475264baf77f9f9331b5e3e0
TeddyNgonga <60339152+TeddyNgonga@users.noreply.github.com>
2020-01-28 11:31:51 +0000

    Add files via upload

---
+++ src/main/java/com/HM/mainFile.java
@@ -0,0 +1,10 @@
+package com.HM;
+
+public class mainFile {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
+}
